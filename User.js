f6bff11f10c200bedb292e54fa29975891a4d2fc
PotatoFleet <supsrasri@gmail.com>
2022-10-08 17:29:59 +0530

    first

---
+++ User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function save(next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
+
+module.exports = new mongoose.model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,103 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const User = require("./User");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost/authdb");
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username })
+      .then((user) => {
+        return done(
+          null,
+          !user || !user.validatePassword(password) ? false : user
+        );
+      })
+      .catch((err) => {
+        done(err);
+      });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    if (err) return done(err);
+    done(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/fail" }),
+  (_req, res) => {
+    res.redirect("/success");
+  }
+);
+
+app.get("/fail", (_req, res) => {
+  res.send("Failed");
+});
+
+app.get("/success", (req, res) => {
+  res.send(req.session);
+});
+
+app.post("/register", async (req, res) => {
+  const usernameFound = await User.findOne({ username: req.body.username });
+
+  if (usernameFound !== null) {
+    res.send({
+      successful: false,
+      message: `User with username "${req.body.username}" already exists`,
+    });
+    return;
+  }
+
+  try {
+    new User({
+      username: req.body.username,
+      password: req.body.password,
+    }).save();
+  } catch (e) {
+    res.send({
+      successful: false,
+      message: "Failed to create user",
+    });
+  }
+
+  res.send({
+    successful: true,
+    message: "Successfully registered",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
